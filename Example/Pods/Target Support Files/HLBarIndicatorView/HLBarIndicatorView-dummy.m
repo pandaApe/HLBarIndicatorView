65d0b6ad40bf0bbce05e0af7316763c4a4ccd982
Wang HaiLong <whailong2010@gmail.com>
2017-06-27 22:30:01 +0800

    Removed Pods folder

--- Example/Pods/Target Support Files/HLBarIndicatorView/HLBarIndicatorView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HLBarIndicatorView : NSObject
-@end
-@implementation PodsDummy_HLBarIndicatorView
-@end
--- Example/Pods/Target Support Files/Pods-HLBarIndicatorView_Example/Pods-HLBarIndicatorView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HLBarIndicatorView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_HLBarIndicatorView_Example
-@end
